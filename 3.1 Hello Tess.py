39318748febd319e1fc8fe2b7e82033f02d05575
Logan-Estridge <140263530+Logan-Estridge@users.noreply.github.com>
2023-07-26 00:10:22 -0500

    Add files via upload

--- 3.1 Hello Tess.py
+++ 3.1 Hello Tess.py
@@ -6,8 +6,10 @@ wn.bgcolor(bg)  # Set the window BG color
 wn.title("Hello, Tess!")  # Set the window title
 
 tess = turtle.Turtle()  # Create a turtle, assign to tess
-tess.color("blue")  # Tell tess to change her color
-tess.pensize(3)  # Tell tess to set her pen width
+cl = input('What color should Tess the turtle be? ')
+tess.color(cl)  # Tell tess to change her color
+ps = int(input('What pensize should Tess be? '))
+tess.pensize(ps)  # Tell tess to set her pen width
 
 tess.forward(50)  # Tell tess to move forward by 50 units
 tess.left(120)  # Tell tess to turn by 120 degrees
